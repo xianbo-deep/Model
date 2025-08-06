678022fa4d236f7b71dd4e089388d10e81318630
BO <2396768163@qq.com>
2025-08-06 12:16:07 +0800

    commit

--- VGG/train.py
+++ VGG/train.py
@@ -19,6 +19,7 @@ train_transform = transforms.Compose([
 test_transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
 ])
 
 
